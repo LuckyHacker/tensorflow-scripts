f15ce9b0649452f78e38a93d60e0464aba95db35
Onni Hakkari <k1631@student.jamk.fi>
2017-10-02 13:55:17 +0300

    Add some average errors and test outputs

--- models/tensorflow_tests/stock_predictor.py
+++ models/tensorflow_tests/stock_predictor.py
@@ -6,7 +6,7 @@ import os
 
 outfile = "prediction.png"
 outfolder = "output"
-infile = "GOOG_alltargets.csv"
+infile = "KNYJF_alltargets.csv"
 infolder = "stock" # ("stock" / "currency")
 
 learning_rate = 0.001
@@ -30,6 +30,7 @@ def prepare_data():
     datasetTrain = datasetNorm[dataset.index < train_length]
     datasetTest = datasetNorm[dataset.index >= train_length]
 
+
     try:
         xTrain = datasetTrain[['Open','High','Low','Close']].as_matrix()
         yTrain = datasetTrain[['OpenTarget','HighTarget','LowTarget','CloseTarget']].as_matrix()
@@ -48,7 +49,7 @@ def prepare_data():
     return xTrain, xTest, yTrain, yTest, test_prices
 
 
-def model():
+def model(nn_mode="lstm"):
     batchX_placeholder = tf.placeholder(dtype=tf.float32,
                                         shape=[None, truncated_backprop_length, num_features],
                                         name='data_ph')
@@ -60,7 +61,12 @@ def model():
     b2 = tf.Variable(   initial_value=np.random.rand(1, num_classes),
                         dtype=tf.float32)
     labels_series = tf.unstack(batchY_placeholder, axis=1)
-    cell = tf.contrib.rnn.LSTMCell(num_units=state_size)
+
+    if nn_mode == "lstm":
+        cell = tf.contrib.rnn.LSTMCell(num_units=state_size)
+    elif nn_mode == "rnn":
+        cell = tf.contrib.rnn.BasicRNNCell(num_units=state_size)
+
     states_series, current_state = tf.nn.dynamic_rnn(   cell=cell,
                                                         inputs=batchX_placeholder,
                                                         dtype=tf.float32)
@@ -82,6 +88,8 @@ def train_and_test( loss, train_step, prediction, last_label, last_state,
     loss_list = []
     test_days_pred_list = []
     test_day_pred_list = []
+    test_days_differences = []
+    test_day_differences = []
 
     with tf.Session() as sess:
         tf.global_variables_initializer().run()
@@ -110,15 +118,19 @@ def train_and_test( loss, train_step, prediction, last_label, last_state,
         # Test len(xTest) days
         for test_idx in range(len(xTest) - truncated_backprop_length):
             if test_idx == 0:
+                testBatchY = yTest[test_idx:test_idx+truncated_backprop_length].reshape((1, truncated_backprop_length, num_classes))
                 testBatchX = xTest[test_idx:test_idx+truncated_backprop_length,:].reshape((1, truncated_backprop_length, num_features))
                 testBatchX = testBatchX.tolist()
             else:
+                testBatchY = yTest[test_idx:test_idx+truncated_backprop_length].reshape((1, truncated_backprop_length, num_classes))
                 test_pred = test_pred[0].tolist()
                 testBatchX[0].append(test_pred)
                 testBatchX[0].pop(0)
 
             test_pred = prediction.eval(feed_dict={batchX_placeholder : testBatchX})
             test_days_pred_list.append(test_pred[-1][0])
+
+            test_days_differences.append(abs(testBatchY[0][-1][0] - test_days_pred_list[-1]))
 
         # Test per day
         for test_idx in range(len(xTest) - truncated_backprop_length):
@@ -131,26 +143,45 @@ def train_and_test( loss, train_step, prediction, last_label, last_state,
             _last_state, _last_label, test_pred = sess.run([last_state, last_label, prediction], feed_dict=feed)
             test_day_pred_list.append(test_pred[-1][0])
 
-    return test_day_pred_list, test_days_pred_list
+            test_day_differences.append(abs(testBatchY[0][-1][0] - test_day_pred_list[-1]))
+
+    return test_day_pred_list, test_days_pred_list, test_day_differences, test_days_differences
 
 
-def plot_prediction(day, days, test_prices):
+def plot_prediction(day, days, test_prices, day_differences, days_differences):
     fig = plt.figure(num=None, figsize=(22, 10), dpi=80, facecolor="white")
     fig.canvas.set_window_title("Stock prediction {}".format(infile))
     ax1 = fig.add_subplot(1, 2, 1)
     ax2 = fig.add_subplot(1, 2, 2)
 
+    day_price_error = sum(day_differences) / len(day_differences) * (dataset["Close"].max() - dataset["Close"].min()) + dataset["Close"].min()
+    days_price_error = sum(days_differences) / len(days_differences) * (dataset["Close"].max() - dataset["Close"].min()) + dataset["Close"].min()
+
+    ax1.set_xlabel("Days")
+    ax1.set_ylabel("Price")
     ax1.set_title("Close price prediction per day")
+    ax1.text(0.5, 0.95, "Average price error: %.6f" % day_price_error,
+                    fontsize=16,
+                    horizontalalignment='center',
+                    verticalalignment='center',
+                    transform=ax1.transAxes)
     ax1.plot(test_prices, label='Price', color='blue')
     ax1.plot(day, label='Predicted',color='red')
     ax1.legend(loc='upper left')
 
+    ax2.set_xlabel("Days")
+    ax2.set_ylabel("Price")
     ax2.set_title("Prediction for {} days".format(len(test_prices)))
+    ax2.text(0.5, 0.95, "Average price error: %.6f" % (days_price_error),
+                    fontsize=16,
+                    horizontalalignment='center',
+                    verticalalignment='center',
+                    transform=ax2.transAxes)
     ax2.plot(test_prices, label='Price', color='blue')
     ax2.plot(days, label='Predicted', color='red')
     ax2.legend(loc='upper left')
 
-    plt.figtext(0.01, 0.5, """Learning Rate = {}\nEpochs = {}\nBatch Size = {}\nTrain Size = {}\nTruncated Backprop = {}\nState Size = {}""".format(
+    plt.figtext(0.005, 0.5, """Learning Rate = {}\nEpochs = {}\nBatch Size = {}\nTrain Size = {}\nTruncated Backprop = {}\nState Size = {}""".format(
                         learning_rate, num_epochs, batch_size, train_size,
                         truncated_backprop_length, state_size), bbox={'facecolor': 'darkblue', 'alpha': 0.5, 'pad': 10},
                         fontsize=11)
@@ -163,8 +194,8 @@ if __name__ == "__main__":
     outfile = infile.split(".")[0] + "_" + outfile
     trainX, testX, trainY, testY, prices = prepare_data()
     loss, train_step, prediction, last_label, last_state, placeholder_x, placeholder_y = model()
-    day, days = train_and_test( loss, train_step, prediction, last_label, last_state,
+    day, days, test_day_differences, test_days_differences = train_and_test( loss, train_step, prediction, last_label, last_state,
                                 placeholder_x, placeholder_y,
                                 trainX, testX, trainY, testY)
 
-    plot_prediction(day, days, prices)
+    plot_prediction(day, days, prices, test_day_differences, test_days_differences)
