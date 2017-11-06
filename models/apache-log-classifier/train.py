31ecb855a34c5234d667fb6b8230a29aa93c4baa
Onni Hakkari <k1631@student.jamk.fi>
2017-11-06 11:24:32 +0200

    Change apache-log-classifer model to keras

--- models/apache-log-classifier/train.py
+++ models/apache-log-classifier/train.py
@@ -1,17 +1,20 @@
-import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.callbacks import TensorBoard
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import time
 
 
+train_size = 0.8
+batch_size = 30
+state_size = 54
 num_features = 27
-truncated_backprop_length = 1
-state_size = 75
 num_classes = 1
-batch_size = 10
-train_size = 0.9
-num_epochs = 100
+dropout = 0.2
 learning_rate = 0.001
+epochs = 5000
 
 csv_file_path = "data/access.csv"
 log_file_path = "data/access.log"
@@ -19,7 +22,6 @@ log_file_path = "data/access.log"
 dataset = pd.read_csv(csv_file_path)
 train_length = int(len(dataset.index) * train_size)
 test_length = int(len(dataset.index) * (1.0 - train_size))
-num_batches = train_length // batch_size // truncated_backprop_length
 
 features = ["date",
             "time",
@@ -51,7 +53,7 @@ features = ["date",
             ]
 
 def prepare_data():
-    #datasetNorm = (dataset - dataset.min()) / (dataset.max() - dataset.min())
+    datasetNorm = (dataset - dataset.min()) / (dataset.max() - dataset.min())
     datasetNorm = dataset.sample(frac=1)
     datasetTrain = datasetNorm[dataset.index < train_length]
     datasetTest = datasetNorm[dataset.index >= train_length]
@@ -66,89 +68,41 @@ def prepare_data():
     return xTrain, yTrain, xTest, yTest, test_ids
 
 
-def model():
-    batchX_placeholder = tf.placeholder(dtype=tf.float32,
-                                        shape=[None, truncated_backprop_length, num_features],
-                                        name='data_ph')
-    batchY_placeholder = tf.placeholder(dtype=tf.float32,
-                                        shape=[None, truncated_backprop_length, num_classes],
-                                        name='target_ph')
-    W2 = tf.Variable(   initial_value=np.random.rand(state_size, num_classes),
-                        dtype=tf.float32)
-    b2 = tf.Variable(   initial_value=np.random.rand(1, num_classes),
-                        dtype=tf.float32)
-    labels_series = tf.unstack(batchY_placeholder, axis=1)
-    cell = tf.contrib.rnn.LSTMCell(num_units=state_size)
-    states_series, current_state = tf.nn.dynamic_rnn(   cell=cell,
-                                                        inputs=batchX_placeholder,
-                                                        dtype=tf.float32)
-    states_series = tf.transpose(states_series, [1,0,2])
-    last_state = tf.gather(params=states_series, indices=states_series.get_shape()[0]-1)
-    last_label = tf.gather(params=labels_series, indices=len(labels_series)-1)
-    weight = tf.Variable(tf.truncated_normal([state_size, num_classes]))
-    bias = tf.Variable(tf.constant(0.1, shape=[num_classes]))
-    prediction = tf.matmul(last_state, weight) + bias
-    loss = tf.reduce_mean(tf.squared_difference(last_label, prediction))
-    train_step = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
-    return (loss, train_step, prediction, last_label,
-            last_state, batchX_placeholder, batchY_placeholder)
+def plot_data(x, y):
+    bins = np.linspace(-2, 2, 10)
+
+    plt.hist(x, bins, alpha=1, label='predicted: 0')
+    plt.hist(y, bins, alpha=1, label='predicted: 1')
+    plt.legend(loc='upper right')
+    plt.show()
 
 
-def train(  loss, train_step, prediction, last_label, last_state,
-            batchX_placeholder, batchY_placeholder, xTrain, yTrain,
-            xTest, yTest):
-    loss_list = []
+def train(x_train, y_train, x_test, y_test):
+    # Network building
+    model = Sequential()
+    model.add(Dense(state_size, input_dim=num_features, activation='relu'))
+    model.add(Dropout(dropout))
+    model.add(Dense(state_size // 2, activation='relu'))
+    model.add(Dropout(dropout))
+    model.add(Dense(state_size // 4, activation='softmax'))
+    model.add(Dropout(dropout))
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
 
-    with tf.Session() as sess:
-        tf.global_variables_initializer().run()
-        print('Train data length: %d' % train_length)
-        print('Test data length: %d' % (test_length+1))
+    tensorboardlog = TensorBoard(log_dir='./keras_log', histogram_freq=0, write_graph=True, write_images=True)
+    history = model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size, callbacks=[tensorboardlog])
 
-        # Train
-        for epoch_idx in range(num_epochs):
-            begin_time = time.time()
-            for batch_idx in range(num_batches):
-                start_idx = batch_idx * truncated_backprop_length
-                end_idx = start_idx + truncated_backprop_length * batch_size
+    losses = history.history["loss"]
+    plt.plot(list(range(len(losses))), losses, "-")
+    plt.show()
 
-                batchX = xTrain[start_idx:end_idx,:].reshape(batch_size,truncated_backprop_length,num_features)
-                batchY = yTrain[start_idx:end_idx].reshape(batch_size,truncated_backprop_length,num_classes)
-
-                _loss, _train_step, _pred, _last_label, _prediction = sess.run(
-                    fetches = [loss, train_step, prediction, last_label, prediction],
-                    feed_dict = {   batchX_placeholder : batchX,
-                                    batchY_placeholder : batchY}
-                )
-
-                loss_list.append(_loss)
-
-            end_time = time.time() - begin_time
-            print('Epoch: %d, loss: %.6f, time: %.2fs' % (epoch_idx + 1, _loss, end_time))
+    score = model.evaluate(x_test, y_test, batch_size=batch_size)
+    print("\nAccuracy: {0:.2f} % ".format(score[1] * 100))
 
 
-        pred_list = []
-        for test_idx in range(len(xTest) - truncated_backprop_length + 1):
-            testBatchX = xTest[test_idx:test_idx+truncated_backprop_length,:].reshape((1, truncated_backprop_length, num_features))
-            testBatchY = yTest[test_idx:test_idx+truncated_backprop_length].reshape((1, truncated_backprop_length, num_classes))
-
-            feed = {batchX_placeholder : testBatchX,
-                    batchY_placeholder : testBatchY}
-
-            _last_state, _last_label, test_pred = sess.run([last_state, last_label, prediction], feed_dict=feed)
-            pred_list.append(test_pred[-1][0])
-
-
-        pred_list = list(map(lambda x: int(round(x)), pred_list))
-        correct_preds = 0
-
-        for i in range(len(yTest)):
-            if yTest[i][0] == pred_list[i]:
-                correct_preds += 1
-
-
-        accuracy = correct_preds / len(yTest) * 100
-        print("Accuracy: {}%".format(accuracy))
-        return pred_list
+    preds = list(map(lambda x: int(round(x[0])), model.predict(x_test)))
+    plot_data([0] * preds.count(0), [1] * preds.count(1))
+    return preds
 
 
 def show_log_lines(test_ids, pred_list):
@@ -164,10 +118,6 @@ def show_log_lines(test_ids, pred_list):
         print("{}    pred: {}".format(line[0], line[1]))
 
 if __name__ == "__main__":
-    loss, train_step, prediction, last_label, last_state, batchX_placeholder, batchY_placeholder = model()
     xTrain, yTrain, xTest, yTest, test_ids = prepare_data()
-    pred_list = train(  loss, train_step, prediction, last_label,
-                        last_state, batchX_placeholder, batchY_placeholder,
-                        xTrain, yTrain, xTest, yTest)
-
+    pred_list = train(xTrain, yTrain, xTest, yTest)
     show_log_lines(test_ids, pred_list)
